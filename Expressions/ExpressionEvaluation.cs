16b08588d280cbe523232f6b8f5ff5e29eab42e3
Dmitrii Orekhov <dmitrii.orekhov@jetbrains.com>
2021-03-21 02:54:43 +0300

    Evaluation

---
+++ Expressions/ExpressionEvaluation.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+
+namespace Expressions
+{
+    public static class ExpressionEvaluation
+    {
+        public static int Evaluate(this IExpression expression, Dictionary<string, int> environment) => expression switch
+        {
+            Literal lit => int.Parse(lit.Value),
+            Variable var => environment[var.Name],
+            BinaryExpression bin => bin.Evaluate(environment),
+            ParenExpression paren => paren.Operand.Evaluate(environment)
+        };
+
+        private static int Evaluate(this BinaryExpression expression, Dictionary<string, int> environment)
+        {
+            var left = expression.Left.Evaluate(environment);
+            var right = expression.Right.Evaluate(environment);
+            return expression.Operator switch
+            {
+                Plus => left + right,
+                Minus => left - right,
+                Mult => left * right,
+                Div => left / right
+            };
+        }
+    }
+}
