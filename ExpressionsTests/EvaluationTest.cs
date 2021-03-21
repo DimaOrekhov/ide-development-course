10327f779152eb2046a143be46d74eaa8f613508
Dmitrii Orekhov <dmitrii.orekhov@jetbrains.com>
2021-03-21 17:25:08 +0300

    Evaluate expressions with variables tests

--- ExpressionsTests/EvaluationTest.cs
+++ ExpressionsTests/EvaluationTest.cs
@@ -7,7 +7,7 @@ namespace ExpressionsTests
     public abstract class AbstractEvaluationTest
     {
         protected abstract int Evaluate(string text, Dictionary<string, int> environment = null);
-        
+
         [Test]
         public void TestTwoOperands()
         {
@@ -17,6 +17,23 @@ namespace ExpressionsTests
             Assert.AreEqual(3, Evaluate("9 / 3"));
         }
 
+        [Test]
+        public void TestTwoOperandsWithVariables()
+        {
+            var env = new Dictionary<string, int>
+            {
+                { "a", 10 },
+                { "b", 2  }
+            };
+            Assert.AreEqual(12, Evaluate("a + b", env));
+            Assert.AreEqual(-8, Evaluate("b - a", env));
+            Assert.AreEqual(8, Evaluate("a - b", env));
+            Assert.AreEqual(20, Evaluate("a *  b", env));
+            Assert.AreEqual(5, Evaluate("a / b", env));
+            Assert.AreEqual(0, Evaluate("b / a", env));
+
+        }
+        
         [Test]
         public void TestTwoOperandsInParens()
         {
@@ -36,6 +53,21 @@ namespace ExpressionsTests
             Assert.AreEqual(9 - 2 - 3,Evaluate("9 - 2 - 3"));
             Assert.AreEqual(8 / 2 / 2, Evaluate("8 / 2 / 2"));
         }
+        
+        [Test]
+        public void TestLeftAssociativityWithVariables()
+        {
+            var env = new Dictionary<string, int>
+            {
+                { "a", 8  },
+                { "b", 3  },
+                { "c", 24 }
+            };
+            Assert.AreEqual(env["b"] - env["a"] - env["c"], 
+                Evaluate("b - a - c", env));
+            Assert.AreEqual(env["c"] / env["a"] / env["b"], 
+                Evaluate("c / a / b", env));
+        }
 
         [Test]
         public void TestParenPriority()
@@ -52,6 +84,20 @@ namespace ExpressionsTests
             Assert.AreEqual(1 + (3) * 4 / ( 3 -  1 - 1), 
                 Evaluate("  1 + (3) * 4 / ( 3 -  1 - 1)  "));
         }
+
+        [Test]
+        public void TestComplicatedExpressionWithVariables()
+        {
+            var env = new Dictionary<string, int>
+            {
+                { "a", 8   },
+                { "c", 3   },
+                { "x", 24  },
+                { "y", 997 }
+            };
+            Assert.AreEqual(env["c"] + (env["x"]) * 4 / (env["y"] - env["a"] - 1),
+                Evaluate("  c + (x) * 4 / ( y -  a - 2)  ", env));
+        }
     }
     
     public class InterpretationTests : AbstractEvaluationTest
@@ -63,6 +109,6 @@ namespace ExpressionsTests
     public class CompilationTests : AbstractEvaluationTest
     {
         protected override int Evaluate(string text, Dictionary<string, int> environment = null) =>
-            (int) Compiler.ParseAndCompileExpression(text).Evaluate();
+            Compiler.ParseAndCompileExpression(text).Evaluate(environment);
     }
 }
