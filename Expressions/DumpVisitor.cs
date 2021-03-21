0e6a525719b9868337f9821fa7ce62c6e00e9b91
Dmitrii Orekhov <dmitrii.orekhov@jetbrains.com>
2021-03-21 03:46:12 +0300

    Parsing tests

--- Expressions/DumpVisitor.cs
+++ Expressions/DumpVisitor.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Text;
 
 namespace Expressions
@@ -6,6 +7,14 @@ namespace Expressions
     {
         private readonly StringBuilder _builder;
 
+        private static string Dump(IOperator @operator) => @operator switch
+        {
+            Plus => "+",
+            Minus => "-",
+            Mult => "*",
+            Div => "/"
+        };
+        
         public DumpVisitor()
         {
             _builder = new StringBuilder();
@@ -13,26 +22,26 @@ namespace Expressions
 
         public void Visit(Literal expression)
         {
-            _builder.Append("Literal(" + expression.Value + ")");
+            _builder.Append("Lit(" + expression.Value + ")");
         }
 
         public void Visit(Variable expression)
         {
-            _builder.Append("Variable(" + expression.Name + ")");
+            _builder.Append("Var(" + expression.Name + ")");
         }
 
         public void Visit(BinaryExpression expression)
         {
-            _builder.Append("Binary(");
+            _builder.Append("Bin(");
             expression.Left.Accept(this);
-            _builder.Append(expression.Operator);
+            _builder.Append(Dump(expression.Operator));
             expression.Right.Accept(this);
             _builder.Append(')');
         }
 
         public void Visit(ParenExpression expression)
         {
-            _builder.Append("Paren(");
+            _builder.Append("Par(");
             expression.Operand.Accept(this);
             _builder.Append(')');
         }
